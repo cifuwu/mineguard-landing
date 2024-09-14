a4e2551f4f68cacd863965bd62e56ca87565d4df
cifuwu <sebastian.cifuentesm@sansano.usm.cl>
2024-09-14 07:30:44 -0300

    xd

--- app/contacto/page.js
+++ app/contacto/page.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import Image from 'next/image'
 import { FaChartLine, FaFileExport, FaMoneyBills, FaPersonCircleQuestion } from 'react-icons/fa6'
-import Mapa from './mapa'
+import Mapa from '../../components/Mapa'
 
 
 export const metadata = {
