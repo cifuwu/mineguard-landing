bd641360e4d0050cc55b1e47c263e69ef17bc23c
cifuwu <sebastian.cifuentesm@sansano.usm.cl>
2024-09-14 19:26:29 -0300

    xd

--- components/Footer/index.jsx
+++ components/Footer/index.jsx
@@ -8,11 +8,9 @@ export default function Footer() {
     <footer className="bg-blue-50/70  mt-20">
       <div className="mx-auto w-full xl:px-10 p-4 py-6 lg:py-8">
         <div className="md:flex md:justify-between">
-          <div className="mb-6 md:mb-0">
-            <Link href="/" className="flex items-center gap-2">
-              <Image src={logo} width={50} height={50} className="h-8 w-8" alt="FlowBite Logo" />
-              <span className="self-center md:text-xl font-semibold whitespace-nowrap">MineGuard</span>
-            </Link>
+          <div className="mb-6 md:mb-0 flex flex-col justify-center items-center gap-1">
+            <Image src={logo} width={85} height={85} className="rounded-full" alt="AridDewellers Logo" />
+            <p className='font-medium text-sm text-gray-700'>Desarrollado por AridDewellers</p>
           </div>
           <div className="grid grid-cols-2 gap-8 sm:gap-10">
               <div>
@@ -42,7 +40,7 @@ export default function Footer() {
       </div>
       <hr className="my-6 border-gray-200 sm:mx-auto  lg:my-8" />
       <div className="sm:flex sm:items-center sm:justify-between">
-        <span className="text-sm text-gray-500 sm:text-center ">Hecho con ❤️ por <Link href="/" className="hover:underline">Cifu</Link>.
+        <span className="text-sm text-gray-500 sm:text-center ">Hecho con ❤️ por <Link href="https://www.linkedin.com/in/cifu/" target='_blank' className="hover:underline">Cifu</Link>.
         </span>
         <div className="flex mt-4 sm:justify-center sm:mt-0">
         
--- components/NavBar/index.jsx
+++ components/NavBar/index.jsx
@@ -1,7 +1,7 @@
 'use client'
 import Link from 'next/link'
 import React, { useEffect, useState } from 'react'
-import logo from '@/public/logo_2.png'
+import logo from '@/public/logo_camion.png'
 import Image from 'next/image'
 import { FaBars } from 'react-icons/fa6'
 import PopoverTelefonos from './PopoverTelefonos'
@@ -32,7 +32,7 @@ export default function NavBar() {
     <nav className={`fixed flex w-full items-center justify-between px-4 lg:px-14 pt-3 pb-2 z-50 ${navbarBackground ? 'backdrop-blur-lg' : 'bg-transparent'} `}>
       <div className="flex items-center">
         <Link href={'/'} className="flex items-center gap-1">
-          <Image src={logo} alt='logo shopingcloud' width={40} height={40}></Image>
+          <Image src={logo} alt='logo mineguard' width={45} height={45}></Image>
           <p className="text-sm font-medium">MineGuard</p>
         </Link>
 
